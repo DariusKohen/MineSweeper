c94bf431580c4383c568034d0b829908437de081
darius.kohen <darius.kohen@epitech.eu>
2020-12-10 18:09:22 +0100

    Milestone 6

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -3,88 +3,102 @@ package com.example.minesweeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.GridLayout;
-import android.widget.GridView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
+import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     GridLayout gl;
+    TextView minesTxt;
+    TextView marksTxt;
     Button resetBtn;
+    Button markBtn;
     MineSweeperCell[] cells;
     boolean gameState = true;
-    int nbMines = 20;
+    boolean markClick = false;
+    int nbMines = 10;
     int nbCols;
     int nbRows;
+    int nbMarks = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gl = (GridLayout) findViewById(R.id.minesgrid);
+        minesTxt = (TextView) findViewById(R.id.minesTxt);
+        marksTxt = (TextView) findViewById(R.id.marksTxt);
+        gl = (GridLayout) findViewById(R.id.minesGrid);
         resetBtn = (Button) findViewById(R.id.reset);
+        markBtn = (Button) findViewById(R.id.markToggle);
 
+        minesTxt.setText(getString(R.string.mines_txt, nbMines));
+        marksTxt.setText(getString(R.string.marks_txt, nbMarks));
         nbCols = gl.getColumnCount();
         nbRows = gl.getRowCount();
         cells = new MineSweeperCell[nbCols * nbRows];
         initGrid();
         dispatchMines();
+        dispatchNumbers();
         sizeGridCell();
-        resetBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetGame();
+        resetBtn.setOnClickListener(v -> resetGame());
+        markBtn.setOnClickListener(v -> {
+            if (gameState) {
+                markClick = !markClick;
+                markBtn.setText((
+                    markClick ? getString(R.string.uncover_btn) :
+                        getString(R.string.mark_btn)
+                ));
             }
         });
     }
 
     private void initGrid() {
         for(int i = 0; i < (nbCols * nbRows); i++) {
-            cells[i] = new MineSweeperCell(this, i % 10, i / 10);
-            cells[i].setOnSelectedListener(new MineSweeperCell.OnSelectedListener() {
-                @Override
-                public boolean OnSelected(MineSweeperCell msc, boolean mine) {
-                    if (mine && gameState) {
-                        gameState = false;
+            cells[i] = new MineSweeperCell(this, i);
+            cells[i].setOnSelectedListener(msc -> {
+                if (!gameState) {
+                    return true;
+                }
+                if (markClick) {
+                    if (msc.isCovered()) {
+                        nbMarks += (msc.isMarked() ? -1 : 1);
+                        marksTxt.setText(getString(R.string.marks_txt, nbMarks));
+                        msc.toggleMark();
+                        if (nbMarks == nbMines) {
+                            checkWin();
+                        }
                         return false;
-                    } else if (!gameState) {
-                        return true;
                     }
-                    return false;
+                } else if (!msc.isMarked()) {
+                    if (msc.isMine()) {
+                        gameOver();
+                        return false;
+                    }
+                    if (!msc.isMine() && msc.getNumber() == 0) {
+                        clickAround(msc.getIndex());
+                    }
                 }
-            });
-            cells[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                }
+                return false;
             });
             gl.addView(cells[i]);
         }
     }
 
     private void sizeGridCell() {
-        gl.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                GridLayout.LayoutParams params = null;
-                int gridWidth = gl.getWidth();
-                int gridHeight = gl.getHeight();
-                int cellMargin = 5;
+        gl.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+            GridLayout.LayoutParams params;
+            int gridWidth = gl.getWidth();
+            int gridHeight = gl.getHeight();
+            int cellMargin = 5;
 
-                for(int i = 0; i < (nbCols * nbRows); i++) {
-                    params = (GridLayout.LayoutParams)cells[i].getLayoutParams();
-                    params.width = (gridWidth/nbCols) - 2*cellMargin;
-                    params.height = (gridHeight/nbRows) - 2*cellMargin;
-                    params.setMargins(cellMargin, cellMargin, cellMargin, cellMargin);
-                    cells[i].setLayoutParams(params);
-                }
+            for (MineSweeperCell cell : cells) {
+                params = (GridLayout.LayoutParams) cell.getLayoutParams();
+                params.width = (gridWidth / nbCols) - 2 * cellMargin;
+                params.height = (gridHeight / nbRows) - 2 * cellMargin;
+                params.setMargins(cellMargin, cellMargin, cellMargin, cellMargin);
+                cell.setLayoutParams(params);
             }
         });
     }
@@ -92,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
     private void dispatchMines() {
         for(int i = 0; i < nbMines; i++) {
             int rand = new Random().nextInt(nbCols * nbRows);
-            if (!cells[rand].getMine()) {
+            if (!cells[rand].isMine()) {
                 cells[rand].setMine();
             } else {
                 --i;
@@ -100,15 +114,86 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void cleargrid() {
-        for(int i = 0; i < (nbCols * nbRows); i++) {
-            cells[i].resetCell();
+    private void dispatchNumbers() {
+        int count = 0;
+
+        for(int i = 0; i < cells.length; i++) {
+            if (!cells[i].isMine()) {
+                for (int y = -1; y < 2; y++) {
+                    for (int x = -1; x < 2; x++) {
+                        if ((i / nbRows + y) >= 0 && (i / nbRows + y) < nbRows &&
+                                (i % nbCols + x) >= 0 && (i % nbCols + x) < nbCols
+                        ) {
+                            count += (cells[(i / nbRows + y) * nbCols + (i % nbCols + x)].isMine() ? 1 : 0);
+                        }
+                    }
+                }
+            }
+            cells[i].setNumber(count);
+            count = 0;
+        }
+    }
+
+    private void clearGrid() {
+        for (MineSweeperCell cell : cells) {
+            cell.resetCell();
         }
     }
 
     private void resetGame() {
-        cleargrid();
+        minesTxt.setText(getString(R.string.mines_txt, nbMines));
+        marksTxt.setText(getString(R.string.marks_txt, 0));
+        markClick = false;
+        nbMarks = 0;
+        markBtn.setText(getString(R.string.mark_btn));
+        clearGrid();
         dispatchMines();
+        dispatchNumbers();
         gameState = true;
+    }
+
+    private void clickAround(int i) {
+        int index;
+
+        for (int y = -1; y < 2; y++) {
+            for (int x = -1; x < 2; x++) {
+                if ((i / nbRows + y) >= 0 && (i / nbRows + y) < nbRows &&
+                        (i % nbCols + x) >= 0 && (i % nbCols + x) < nbCols
+                ) {
+                    index = (i / nbRows + y) * nbCols + (i % nbCols + x);
+                    if (cells[index].isCovered()) {
+                        cells[index].reveal();
+                        if (cells[index].getNumber() == 0) {
+                            clickAround(index);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void gameOver() {
+        gameState = false;
+        // Reveal mines
+        for (MineSweeperCell cell : cells) {
+            if (cell.isMine()) {
+                cell.reveal();
+            }
+        }
+        minesTxt.setText(getString(R.string.lose_txt1));
+        marksTxt.setText(getString(R.string.lose_txt2));
+    }
+
+    private void checkWin() {
+        for (MineSweeperCell cell : cells) {
+            if ((cell.isMine() && !cell.isMarked()) ||
+                    (cell.isMarked() && !cell.isMine())
+            ) {
+                return;
+            }
+        }
+        gameState = false;
+        minesTxt.setText(getString(R.string.win_txt1));
+        marksTxt.setText(getString(R.string.win_txt2));
     }
 }
--- app/src/main/java/com/example/minesweeper/MineSweeperCell.java
+++ app/src/main/java/com/example/minesweeper/MineSweeperCell.java
@@ -5,31 +5,31 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class MineSweeperCell extends View {
     public interface OnSelectedListener {
-        boolean OnSelected(MineSweeperCell msc, boolean mine);
+        boolean OnSelected(MineSweeperCell msc);
     }
 
-    private Paint txt;
-    private boolean touchOn;
+    private boolean downTouch;
+    private Paint txtStyle;
+    private int nbr;
+    private boolean uncovered;
     private boolean mine;
+    private boolean marked;
     private OnSelectedListener selectedListener;
-    private int x = 0;
-    private int y = 0;
+    private int index;
 
     public MineSweeperCell(Context ctx) {
         super(ctx);
         init();
     }
 
-    public MineSweeperCell(Context ctx, int x, int y) {
+    public MineSweeperCell(Context ctx, int index) {
         super(ctx);
-        this.x = x;
-        this.y = y;
+        this.index = index;
         init();
     }
 
@@ -44,8 +44,14 @@ public class MineSweeperCell extends View {
     }
 
     private void init() {
-        touchOn = false;
+        downTouch = false;
+        uncovered = false;
         mine = false;
+        marked = false;
+        txtStyle = new Paint(Paint.ANTI_ALIAS_FLAG);
+        txtStyle.setStyle(Paint.Style.FILL);
+        txtStyle.setTextSize(80);
+        txtStyle.setFakeBoldText(true);
     }
 
     @Override
@@ -56,34 +62,71 @@ public class MineSweeperCell extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if (touchOn) {
-            if (mine && txt != null) {
+        if (uncovered) {
+            if (mine && txtStyle != null) {
                 canvas.drawColor(Color.RED);
+                txtStyle.setColor(Color.BLACK);
                 canvas.drawText("M",
-                    this.getLayoutParams().width / 2 - 35,
-                    this.getLayoutParams().height / 2 + 30,
-                    txt
+                    (float) this.getLayoutParams().width / 2 - 35,
+                    (float) this.getLayoutParams().height / 2 + 30,
+                    txtStyle
                 );
-
+            } else if (nbr != 0 && txtStyle != null) {
+                canvas.drawColor(Color.LTGRAY);
+                txtStyle.setColor(Color.GRAY);
+                canvas.drawText(String.valueOf(nbr),
+                    (float) this.getLayoutParams().width / 2 - 25,
+                    (float) this.getLayoutParams().height / 2 + 30,
+                    txtStyle
+                );
             } else {
                 canvas.drawColor(Color.LTGRAY);
             }
+        } else if (marked) {
+            canvas.drawColor(Color.YELLOW);
         } else {
             canvas.drawColor(Color.BLACK);
         }
     }
 
     @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downTouch = true;
+                return true;
+            case MotionEvent.ACTION_UP:
+                if (downTouch) {
+                    downTouch = false;
+                    performClick();
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    @Override
     public boolean performClick() {
+        boolean tmpMarked = marked;
+
         if (selectedListener != null) {
-            if (selectedListener.OnSelected(this, mine)) {
+            if (selectedListener.OnSelected(this)) {
                 return true;
             }
         }
         super.performClick();
-        touchOn = !touchOn;
+        if (!marked && !tmpMarked) {
+            uncovered = true;
+        }
         invalidate();
         return true;
+    }
+
+    public void reveal() {
+        uncovered = true;
+        invalidate();
     }
 
     public void setOnSelectedListener(OnSelectedListener listener) {
@@ -91,29 +134,42 @@ public class MineSweeperCell extends View {
     }
 
     public void resetCell() {
-        touchOn = false;
+        uncovered = false;
+        marked = false;
         mine = false;
+        nbr = 0;
     }
 
-    public int getPosx() {
-        return x;
-    }
-
-    public int getPosY() {
-        return y;
-    }
-
-    public boolean getMine() {
-        return mine;
+    public int getIndex() {
+        return index;
     }
 
     public void setMine() {
         mine = true;
-        txt = new Paint(Paint.ANTI_ALIAS_FLAG);
-        txt.setColor(Color.BLACK);
-        txt.setStyle(Paint.Style.FILL);
-        txt.setTextSize(80);
-        txt.setFakeBoldText(true);
+    }
+
+    public boolean isMine() {
+        return mine;
+    }
+
+    public void setNumber(int nbr) {
+        this.nbr = nbr;
+    }
+
+    public int getNumber() {
+        return nbr;
+    }
+
+    public boolean isCovered() {
+        return !uncovered;
+    }
+
+    public void toggleMark() {
+        marked = !marked;
+    }
+
+    public boolean isMarked() {
+        return marked;
     }
 
 }
